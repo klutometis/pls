e88a60c1fd6d7c66ce82a33be3d0f91f2a0e85c7
Peter Danenberg <pcd@roxygen.org>
2013-12-04 02:55:11 -0600

    Use `with'.

--- import.py
+++ import.py
@@ -5,23 +5,16 @@ import csv
 import psycopg2
 import os
 
-db = psycopg2.connect(host=os.environ.get('PLS_HOST', 'localhost'),
-                      user=os.environ.get('PLS_USER', os.environ.get('USER')),
-                      password=os.environ.get('PLS_PASSWORD', ''),
-                      dbname=os.environ.get('PLS_DB', os.environ.get('USER')))
-cursor = db.cursor()
+with psycopg2.connect(dbname='pls') as db:
+    with db.cursor() as cursor:
+        cursor.execute('insert into stats (imported) select 0 where not exists (select 1 from stats where date = current_date)')
 
-# Race condition: overnight. Would we had redis!
-cursor.execute('insert into stats (imported) select 0 where not exists (select 1 from stats where date = current_date)')
+    reader = csv.DictReader(sys.stdin)
+    for row in reader:
+        question_id = row['ExQbId']
+        student_id = row['Name/ID']
+        outcome = 1 if row['Correct?'] == 'Yes' else 0
 
-reader = csv.DictReader(sys.stdin)
-for row in reader:
-    question_id = row['ExQbId']
-    student_id = row['Name/ID']
-    outcome = 1 if row['Correct?'] == 'Yes' else 0
-
-    cursor.execute('update stats set imported = imported + 1 where date = current_date and not exists (select 1 from data where question_id = %s and student_id = %s)', (question_id, student_id))
-    cursor.execute('insert into data (question_id, student_id, outcome) select %s, %s, %s where not exists (select 1 from data where question_id = %s and student_id = %s)', (question_id, student_id, outcome, question_id, student_id))
-
-db.commit()
-db.close()
+        with db.cursor() as cursor:
+            cursor.execute('update stats set imported = imported + 1 where date = current_date and not exists (select 1 from data where question_id = %s and student_id = %s)', (question_id, student_id))
+            cursor.execute('insert into data (question_id, student_id, outcome) select %s, %s, %s where not exists (select 1 from data where question_id = %s and student_id = %s)', (question_id, student_id, outcome, question_id, student_id))
